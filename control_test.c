a0f1ab922ecb55bfd1c37ea407fe94788e32fe75
Patricio Paez <pp@pp.com.mx>
2013-02-15 21:05:08 -0600

    Start test_two_chars_to_byte

--- control_test.c
+++ control_test.c
@@ -1,8 +1,13 @@
 #include <assert.h>
 #include <stdbool.h>
 
+static void test_two_chars_to_byte() {
+ int byte;
+ byte = two_chars_to_byte("31");
+ assert( byte == 49 && "test_two_chars_to_byte" );
+}
 
 int main(void) {
  assert( false && "First unit test" );
- return 0;
+ return(0);
 }
