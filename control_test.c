eff9adbcc7553ce9a28769546403447a50bb5430
Patricio Paez <pp@pp.com.mx>
2013-02-16 15:43:28 -0600

    Created test_transfer_packet

--- control_test.c
+++ control_test.c
@@ -9,7 +9,17 @@ static void test_two_chars_to_byte() {
  assert( byte == 49 && "test_two_chars_to_byte" );
 }
 
+static void test_transfer_packet() {
+ int key_byte = 49, modifier_byte = 0;
+ int shift_out_bit_ncalled = 0;
+ int shift_in_bit_ncalled = 0;
+ transfer_packet(key_byte, modifier_byte);
+ assert(shift_out_bit_ncalled == 19 && "test_transfer_packet");
+ assert(shift_in_bit_ncalled == 6 && "test_transfer_packet");
+}
+
 int main(void) {
  test_two_chars_to_byte();
+ test_transfer_packet();
  return(0);
 }
