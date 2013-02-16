b173919418c3afbb6145e7ca022a036f72461f9d
Patricio Paez <pp@pp.com.mx>
2013-02-16 16:05:53 -0600

    Stubs of shift_out/in_bit
    - The test fails

--- control.c
+++ control.c
@@ -9,5 +9,16 @@ int two_chars_to_byte(char * two_chars) {
  return byte;
 }
 
+void shift_out_bit( int bit) {
+}
+
+void shift_in_bit( int bit) {
+}
+
+
 void transfer_packet(int key_byte, int modifier_byte) {
+ int bit = 0;
+ int i;
+ for (i=0; i++; i<19) shift_out_bit(bit);
+ for (i=0; i++; i<6) shift_in_bit(bit);
 }
