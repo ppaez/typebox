5d3eb25379f65e0a9c7843aceb54e105ace75a38
Patricio Paez <pp@pp.com.mx>
2013-02-16 20:23:43 -0600

    Mock two functions

--- control.h
+++ control.h
@@ -1,4 +1,8 @@
 
 int two_chars_to_byte(char * two_chars);
 
+void shift_out_bit( int bit);
+
+void shift_in_bit( int bit);
+
 void transfer_packet(int key_byte, int modifier_byte);
--- control_test.c
+++ control_test.c
@@ -13,9 +13,29 @@ static void test_transfer_packet() {
  int key_byte = 49, modifier_byte = 0;
  int shift_out_bit_ncalled = 0;
  int shift_in_bit_ncalled = 0;
+ void (*ori_shift_out_bit)();
+ void (*ori_shift_in_bit)();
+
+ void mock_shift_out_bit( int bit ) {
+  shift_out_bit_ncalled++;
+ }
+
+ void mock_shift_in_bit( int bit ) {
+  shift_in_bit_ncalled++;
+ }
+
+ ori_shift_out_bit = &shift_out_bit;
+ ori_shift_in_bit = &shift_in_bit;
+
+ shift_out_bit = mock_shift_out_bit;
+ shift_in_bit = mock_shift_in_bit;
+
  transfer_packet(key_byte, modifier_byte);
  assert(shift_out_bit_ncalled == 19 && "test_transfer_packet");
  assert(shift_in_bit_ncalled == 6 && "test_transfer_packet");
+
+ shift_out_bit = ori_shift_out_bit;
+ shift_in_bit = ori_shift_in_bit;
 }
 
 int main(void) {
