8de7aa2c37d1540fc2d625df8aac124ac86be412
Patricio Paez <pp@pp.com.mx>
2013-02-20 18:10:00 -0600

    Fixed the expression order

--- control.c
+++ control.c
@@ -31,6 +31,6 @@ void shift_in_bit( int bit) {
 void transfer_packet(int key_byte, int modifier_byte) {
  int bit = 0;
  int i;
- for (i=0; i++; i<19) shift_out_bit(bit);
- for (i=0; i++; i<6) shift_in_bit(bit);
+ for (i=0; i<19; i++) shift_out_bit(bit);
+ for (i=0; i<6; i++) shift_in_bit(bit);
 }
