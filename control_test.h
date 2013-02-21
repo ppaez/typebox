08ae2c5242e6fc635f3864db9c45ad13d98afd30
Patricio Paez <pp@pp.com.mx>
2013-02-20 19:09:50 -0600

    Added parameter names and include

--- control_test.h
+++ control_test.h
@@ -1,3 +1,5 @@
+#include <inttypes.h>
+
 #define HIGH 0x1
 #define LOW  0x0
 
@@ -5,8 +7,8 @@
 #define OUTPUT 0x1
 #define INPUT_PULLUP 0x2
 
-void pinMode(uint8_t, uint8_t);
-void digitalWrite(uint8_t, uint8_t);
-int digitalRead(uint8_t);
+void pinMode(uint8_t pin, uint8_t mode);
+void digitalWrite(uint8_t pin, uint8_t value);
+int digitalRead(uint8_t pin);
 
 void delay(unsigned long);
