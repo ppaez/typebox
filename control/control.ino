13c1e90c5a974a8217f4c630683dd8eca8b5389c
Patricio Paez <pp@pp.com.mx>
2013-02-21 22:21:56 -0600

    Use the easyinput functions
    - control.c and control.h were copied
      into control/ as easyinput.c
      and easyinput.h

--- control/control.ino
+++ control/control.ino
@@ -1,3 +1,5 @@
+#include "easyinput.h"
+
 int ledPin = 13;
 
 void setup() {
@@ -23,6 +25,23 @@ void loop() {
   Serial.print(chars[1]);
   Serial.print(chars[2]);
   Serial.println(chars[3]);
+
+  int key_byte, modifier_byte;
+  char key_chars[2] = "";
+  key_chars[0] = chars[0];
+  key_chars[1] = chars[1];
+  key_chars[2] = '\0';
+
+  char modifier_chars[2] = "";
+  modifier_chars[0] = chars[2];
+  modifier_chars[1] = chars[3];
+  modifier_chars[2] = '\0';
+
+  key_byte = two_chars_to_byte(key_chars);
+  modifier_byte = two_chars_to_byte(modifier_chars);
+
+  transfer_packet(key_byte, modifier_byte);
+
   delay(1000);
   digitalWrite(ledPin, LOW);
 }
